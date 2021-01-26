79311b4a6d6d3369da154abcbdf88fc0fcf094f3
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-26 15:29:59 -0500

    basic styling layout

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -12,12 +12,13 @@ import {
 @Component({
   selector: 'app-showcase',
   templateUrl: 'showcase.component.html',
+  styleUrls: ['showcase.component.scss'],
 })
 export class ShowcaseComponent implements AfterViewInit {
   @ViewChild('component', { static: true, read: ViewContainerRef })
   component!: ViewContainerRef;
 
-  @Input() featureName!: string;
+  @Input() demoName!: string;
 
   typescript!: string;
   template!: string;
@@ -32,21 +33,24 @@ export class ShowcaseComponent implements AfterViewInit {
   async renderComponent() {
     // Import feature module
     const module = await import(
-      `../demos/${this.featureName}/${this.featureName}.module`
+      `../demos/${this.demoName}/${this.demoName}.module`
     );
-    // Get module name
-    const [, moduleName] = Object.getOwnPropertyNames(module);
+    // Generate names
+    const moduleName = `${capitalize(this.demoName)}Module`;
+    const componentName = `${capitalize(this.demoName)}Component`;
     // Compile module and component
     const compiled = await this.compiler.compileModuleAndAllComponentsAsync(
       module[moduleName]
     );
-    // Get module factory reference
+    // Create module factory
     const moduleRef = compiled.ngModuleFactory.create(this.injector);
     // Get component factory
-    const [factory] = compiled.componentFactories;
+    const factory = compiled.componentFactories.find(
+      (factory) => factory.componentType.name === componentName
+    );
     // Create and inject component
     this.component.createComponent(
-      factory as ComponentFactory<any>,
+      factory as ComponentFactory<typeof componentName>,
       undefined,
       this.injector,
       undefined,
@@ -55,17 +59,21 @@ export class ShowcaseComponent implements AfterViewInit {
   }
 
   async renderSource() {
-    // Load typescript component source code
+    // Load component typescript source code
     const typescriptSource = await import(
-      `!!raw-loader!../demos/${this.featureName}/${this.featureName}.component.ts`
+      `!!raw-loader!../demos/${this.demoName}/${this.demoName}.component.ts`
     );
     this.typescript = typescriptSource.default.substring(
       typescriptSource.default.indexOf('export class')
     );
-    // Load angular template source code
+    // Load component template source code
     const templateSource = await import(
-      `!!raw-loader!../demos/${this.featureName}/${this.featureName}.component.html`
+      `!!raw-loader!../demos/${this.demoName}/${this.demoName}.component.html`
     );
     this.template = templateSource.default;
   }
 }
+
+const capitalize = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
