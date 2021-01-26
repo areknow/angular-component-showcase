db1089760b91683cd636c13195fbdfc98d8a08c0
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-26 15:49:15 -0500

    added syntax coloring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { AppComponent } from './app.component';
 import { ShowcaseModule } from './showcase/showcase.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ShowcaseModule],
+  imports: [BrowserModule, ShowcaseModule, HighlightModule],
   bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      },
+    },
+  ],
 })
 export class AppModule {}
--- src/app/showcase/showcase.module.ts
+++ src/app/showcase/showcase.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HighlightModule } from 'ngx-highlightjs';
 import { ShowcaseComponent } from './showcase.component';
 
 @NgModule({
   declarations: [ShowcaseComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HighlightModule],
   exports: [ShowcaseComponent],
 })
 export class ShowcaseModule {}
