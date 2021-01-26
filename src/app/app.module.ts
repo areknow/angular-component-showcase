6435bb63868e964564d8f26ced784bb948eb65db
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-26 18:17:48 -0500

    added material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShowcaseModule } from './showcase/showcase.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ShowcaseModule],
+  imports: [BrowserModule, ShowcaseModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/demos/button/button.component.ts
+++ src/app/demos/button/button.component.ts
@@ -2,7 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'button.component.html',
+  styles: [
+    `
+      button:not(:last-child) {
+        margin-right: 15px;
+      }
+    `,
+  ],
 })
 export class ButtonComponent {
-  text = 'click me';
+  buttons = [
+    {
+      label: 'Basic',
+      disabled: false,
+    },
+    {
+      label: 'Primary',
+      color: 'primary',
+      disabled: false,
+    },
+    {
+      label: 'Accent',
+      color: 'accent',
+      disabled: false,
+    },
+    {
+      label: 'Warn',
+      color: 'warn',
+      disabled: false,
+    },
+    {
+      label: 'Disabled',
+      disabled: true,
+    },
+  ];
 }
