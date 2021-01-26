395a5a02b6347a47210329073a43db5a57843ba9
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-26 17:09:50 -0500

    clean up

--- src/app/showcase/showcase.module.ts
+++ src/app/showcase/showcase.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HighlightModule } from 'ngx-highlightjs';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { ShowcaseComponent } from './showcase.component';
 
 @NgModule({
   declarations: [ShowcaseComponent],
   imports: [BrowserModule, HighlightModule],
   exports: [ShowcaseComponent],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      },
+    },
+  ],
 })
 export class ShowcaseModule {}
